c1dc62512bbb689521b6c49ba66feb7234a34e38
Chris Allen <chris.j.allen1979@gmail.com>
2025-11-18 12:21:38 +0000

    Automated conversion from geerlingguy.java Ansible role

---
+++ examples/init.pp
@@ -0,0 +1,9 @@
+# Example usage of paw_ansible_role_java
+
+# Simple include with default parameters
+include paw_ansible_role_java
+
+# Or with custom parameters:
+# class { 'paw_ansible_role_java':
+#   java_home => undef,
+# }
